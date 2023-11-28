67761d50a5c26449b9f9d9fd5dc2152beaf65a4b
Vehpi Yildirim <66426643+vehpi@users.noreply.github.com>
2023-11-28 16:10:36 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from xpp2python import *
+from model import *
+from dynamics import *
+
+def main():
+
+	info=read_info('model.ode.pars')
+	
+	sol=simul(odefun=odde, tspan=[t0,tend], y0=y0, pars=pars,info=info, fign=1, keep_old=False)
+	
+	pars_temp=pars.copy()
+	pars_temp['inc_i1']=0.75
+	pars_temp['it1']=300
+	
+	sol=simul(odefun=odde, tspan=[t0,tend], y0=y0, pars=pars_temp,info=info, fign=1, keep_old=True)
+	
+	bif_fig(bif_name='bifurcation_data.dat', pars=pars)
+	
+	bif_fig(bif_name='bifurcation_data.dat', pars=pars, dec_bound=True)
+	
+	ani=animation(fignum=4)
+	
+	ani2=animation(fignum=5,dec_bound=True)
+	
+	outputs(fn=12)
+	
+	outputs(fn=13)
+
+if __name__=='__main__':
+	main()
